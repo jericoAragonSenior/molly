9851a7299f35aaccbfc4e82538cb9776a7e1bf32
jericoAragonSenior <jericoaragon.senior@gmail.com>
2024-12-31 03:57:11 -0800

    update header style for response

--- src/widgets/Header/ui/HeaderDesktop.tsx
+++ src/widgets/Header/ui/HeaderDesktop.tsx
@@ -14,15 +14,18 @@ export const HeaderDesktop: FC<HeaderDesktopProps> = ({ className }) => {
 	return (
 		<header className={clsx(className, 'flex items-center justify-between gap-2')}>
 			<HeaderNav />
-			<Link
-				href='/'
-				className='shrink-0 self-start w-[386px] h-[92px] 3xl:w-[300px] 3xl:h-[72px] xl:ml-auto'
-				style={{
-					backgroundImage: 'url(/images/logo-header.svg)',
-					backgroundSize: 'contain',
-					backgroundRepeat: 'no-repeat'
-				}}
-			></Link>
+			<div className='flex flex-1 justify-center'>
+				<Link
+					href='/'
+					className='w-[386px] h-[92px] 3xl:w-[300px] 3xl:h-[72px] flex justify-center items-center'
+					style={{
+						backgroundImage: 'url(/images/logo-header.svg)',
+						backgroundSize: 'contain',
+						backgroundRepeat: 'no-repeat'
+					}}
+				></Link>
+			</div>
+
 			<HeaderBar />
 		</header>
 	)
